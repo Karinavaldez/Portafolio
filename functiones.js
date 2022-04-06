4ddacd8e4efd448cdd193bfbae3068970d9c5c28
Karina Valdez <95250841+Karinavaldez@users.noreply.github.com>
2022-04-06 18:09:42 -0300

    Create functiones.js

---
+++ functiones.js
@@ -0,0 +1,6 @@
+const toggleMenuElement = document.getElementById('toggle_menu');
+const mainMenuElement = document.getElementById('main_menu');
+
+    toggleMenuElement.addEventListener('click', () => {
+        mainMenuElement.classList.toggle('nav_menu--show');
+    }) ;
